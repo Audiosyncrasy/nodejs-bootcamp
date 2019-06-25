c6e585b116fd0608a8ffc63d131582408870d9c1
Audiosyncrasy <jerm.killer2002@gmail.com>
2019-06-24 18:28:55 -0700

    complete route basics and mustache pages

--- route-basics/app.js
+++ route-basics/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // setting up body-parser to parse json
 app.use(bodyParser.json());
